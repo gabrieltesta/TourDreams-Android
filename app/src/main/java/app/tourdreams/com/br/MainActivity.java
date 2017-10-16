0d0020b2cc23cd874d6cb2803ade66ae0d0580f0
BARBARA VENTORIM MOREIRA <16165823@CFP127ALUNO.LOCAL>
2017-10-16 14:04:39 -0200

    1.3.1 Fixes

--- app/src/main/java/app/tourdreams/com/br/MainActivity.java
+++ app/src/main/java/app/tourdreams/com/br/MainActivity.java
@@ -93,6 +93,9 @@ public class MainActivity extends AppCompatActivity
             case R.id.nav_nossosparceiros:
                 startActivity(new Intent(this, NossosParceiros.class));
                 break;
+            case R.id.nav_conhecaseudestino:
+                startActivity(new Intent(this, ConhecaseuDestino.class));
+                break;
         }
 
 
